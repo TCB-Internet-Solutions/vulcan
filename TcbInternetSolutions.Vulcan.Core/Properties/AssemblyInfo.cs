8f67bddfa3ff1ef7c72c48b137b6b1ec31deaaa3
Brad McDavid <brad@wsol.com>
2017-12-11 11:34:54 -0500

    updated minor versions for testing packages.

--- TcbInternetSolutions.Vulcan.Core/Properties/AssemblyInfo.cs
+++ TcbInternetSolutions.Vulcan.Core/Properties/AssemblyInfo.cs
@@ -2,15 +2,10 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("TcbInternetSolutions.Vulcan.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TcbInternetSolutions.Vulcan.Core")]
 [assembly: AssemblyCopyright("Copyright © {year}")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("7f0c58f4-4fc7-4ab4-852f-b1ac649dbb38")]
-[assembly: AssemblyVersion("2.3.3")]
-[assembly: AssemblyFileVersion("2.3.3")]
-[assembly: AssemblyInformationalVersion("2.3.3 Build: {build} Commit Hash: {commit}")]
+[assembly: AssemblyVersion("2.4.0")]
+[assembly: AssemblyFileVersion("2.4.0")]
+[assembly: AssemblyInformationalVersion("2.4.0 Build: {build} Commit Hash: {commit}")]
