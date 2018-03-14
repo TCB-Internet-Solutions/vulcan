cbc025dcdfd7345e5c98c5aaeca0e35025696f9a
Brad McDavid <bmcdavid@wsol.com>
2018-03-14 09:45:51 -0400

    migrated hotfix from 2x branch.

--- TcbInternetSolutions.Vulcan.Core/Properties/AssemblyInfo.cs
+++ TcbInternetSolutions.Vulcan.Core/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("7f0c58f4-4fc7-4ab4-852f-b1ac649dbb38")]
 [assembly: AssemblyVersion("5.0.1")]
 [assembly: AssemblyFileVersion("5.0.1")]
-[assembly: AssemblyInformationalVersion("5.0.1-es5001 Build: {build} Commit Hash: {commit}")]
+[assembly: AssemblyInformationalVersion("5.0.1-es5002 Build: {build} Commit Hash: {commit}")]
